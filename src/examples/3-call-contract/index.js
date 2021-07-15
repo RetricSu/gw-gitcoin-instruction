259e7cea385de11e50c3cc8a5042c9206073355e
Daniel Kmak <neverkuzi@gmail.com>
2021-07-15 12:57:09 +0200

    Separate call to contract to read and write call.

--- src/examples/3-call-contract/index.js
+++ src/examples/3-call-contract/index.js
@@ -1,11 +1,18 @@
 const Web3 = require('web3');
 const { PolyjuiceHttpProvider, PolyjuiceAccounts } = require("@polyjuice-provider/web3");
 
+/**
+ * BEFORE USING THIS SCRIPT MAKE SURE TO REPLACE:
+ * - <YOUR_CONTRACT_ABI>
+ * - <YOUR_CONTRACT_ADDRESS>
+ * - CONTRACT_ADDRESS variable value
+ * - YOUR_READ_FUNCTION_NAME method name
+ * - YOUR_WRITE_FUNCTION_NAME method name
+ */
+
 const ACCOUNT_PRIVATE_KEY = '0xd9066ff9f753a1898709b568119055660a77d9aae4d7a4ad677b8fb3d2a571e5';
 const CONTRACT_ABI = '<YOUR_CONTRACT_ABI>';
-const CONTRACT_ADDRESS = '<YOUR_CONTRACT_ADDRESS';
-// const CONTRACT_ABI = '[     {       "inputs": [],       "stateMutability": "payable",       "type": "constructor"     },     {       "inputs": [         {           "internalType": "uint256",           "name": "x",           "type": "uint256"         }       ],       "name": "set",       "outputs": [],       "stateMutability": "payable",       "type": "function"     },     {       "inputs": [],       "name": "get",       "outputs": [         {           "internalType": "uint256",           "name": "",           "type": "uint256"         }       ],       "stateMutability": "view",       "type": "function"     }   ]';
-// const CONTRACT_ADDRESS = '<YOUR_CONTRACT_ADDRESS';
+const CONTRACT_ADDRESS = '<YOUR_CONTRACT_ADDRESS>';
 
 const GODWOKEN_RPC_URL = 'http://godwoken-testnet-web3-rpc.ckbapp.dev';
 const polyjuiceConfig = {
@@ -25,33 +32,42 @@ web3.eth.accounts = new PolyjuiceAccounts(polyjuiceConfig);
 const account = web3.eth.accounts.wallet.add(ACCOUNT_PRIVATE_KEY);
 web3.eth.Contract.setProvider(provider, web3.eth.accounts);
 
+async function readCall() {
+    const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+
+    const callResult = await contract.methods.YOUR_READ_FUNCTION_NAME().call();
+
+    console.log(`Read call result: ${callResult}`);
+}
+
+async function writeCall() {
+    const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+
+    const tx = await contract.methods.YOUR_WRITE_FUNCTION_NAME().send(
+        {
+            from: account.address,
+            to: '0x' + new Array(40).fill(0).join(''),
+            gas: 6000000,
+            gasPrice: '0',
+        }
+    );
+
+    tx.on('transactionHash', hash => console.log(`Transaction hash: ${hash}`));
+
+    const receipt = await tx;
+
+    console.log('Write call transaction receipt: ', receipt);
+}
+
 (async () => {
     const balance = BigInt(await web3.eth.getBalance(account.address));
 
     if (balance === 0n) {
-        console.log(`Insufficient balance. Can't issue a smart-contract call. Please deposit funds to your Ethereum address: ${deployerAccount.address}`);
+        console.log(`Insufficient balance. Can't issue a smart-contract call. Please deposit funds to your Ethereum address: ${account.address}`);
         return;
     }
 
-    console.log(`Calling contract...`);
-
-    const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
-    console.log(await contract.methods.YOUR_FUNCTION_NAME().call());
-
-
-    // const tx = new web3.eth.Contract(CompiledContractArtifact.abi).deploy({
-    //     data: getBytecodeFromArtifact(CompiledContractArtifact),
-    //     arguments: []
-    // }).send({
-    //     from: deployerAccount.address,
-    //     to: '0x' + new Array(40).fill(0).join(''),
-    //     gas: 6000000,
-    //     gasPrice: '0',
-    // });
-
-    // tx.on('transactionHash', hash => console.log(`Transaction hash: ${hash}`));
-
-    // const receipt = await tx;
-
-    // console.log(receipt);
+    console.log('Calling contract...');
+    await readCall();
+    await writeCall();
 })();
