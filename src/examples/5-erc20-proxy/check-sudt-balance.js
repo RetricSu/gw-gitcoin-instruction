9b9f5542fd2f42d330eaa9746c6d256e8473518e
Daniel Kmak <neverkuzi@gmail.com>
2021-07-21 19:38:37 +0200

    Use nervos-godwoken-integration package.

--- src/examples/5-erc20-proxy/check-sudt-balance.js
+++ src/examples/5-erc20-proxy/check-sudt-balance.js
@@ -1,6 +1,7 @@
 const Web3 = require('web3');
 const { PolyjuiceHttpProvider } = require("@polyjuice-provider/web3");
-const { ethAddressToGodwokenShortAddress } = require('./utils/address');
+const { AddressTranslator } = require('nervos-godwoken-integration');
+
 const CompiledContractArtifact = require(`./build/contracts/ERC20.json`);
 
 const ETHEREUM_ADDRESS = '<YOUR_ETHEREUM_ADDRESS>';
@@ -22,7 +23,8 @@ const web3 = new Web3(provider);
 
 (async () => {
     console.log(`Using Ethereum address: ${ETHEREUM_ADDRESS}`);
-    const polyjuiceAddress = ethAddressToGodwokenShortAddress(ETHEREUM_ADDRESS);
+    const addressTranslator = new AddressTranslator();
+    const polyjuiceAddress = addressTranslator.ethAddressToGodwokenShortAddress(ETHEREUM_ADDRESS);
     console.log(`Corresponding Polyjuice address: ${polyjuiceAddress}`);
 
     console.log(`Checking SUDT balance...`);
