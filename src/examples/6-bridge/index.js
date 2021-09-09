ef30c6559ee2d0ba99fd5f54f215ac04fd5a6bb2
Daniel Kmak <neverkuzi@gmail.com>
2021-09-09 15:58:45 +0200

    Support M1 Macs in Task 6.

--- src/examples/6-bridge/index.js
+++ src/examples/6-bridge/index.js
@@ -4,15 +4,12 @@ const { AddressTranslator } = require('nervos-godwoken-integration');
 
 const ETHEREUM_ADDRESS = '<YOUR_ETHEREUM_ADDRESS>';
 
-const GODWOKEN_RPC_URL = 'https://godwoken-testnet-web3-rpc.ckbapp.dev';
 const polyjuiceConfig = {
-    rollupTypeHash: '0x4cc2e6526204ae6a2e8fcf12f7ad472f41a1606d5b9624beebd215d780809f6a',
-    ethAccountLockCodeHash: '0xdeec13a7b8e100579541384ccaf4b5223733e4a5483c3aec95ddc4c1d5ea5b22',
-    web3Url: GODWOKEN_RPC_URL
+    web3Url: 'https://godwoken-testnet-web3-rpc.ckbapp.dev'
 };
   
 const provider = new PolyjuiceHttpProvider(
-    GODWOKEN_RPC_URL,
+    polyjuiceConfig.web3Url,
     polyjuiceConfig,
 );
 
